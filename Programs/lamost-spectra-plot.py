a9d5d1a48df7f99aefa69f2ba6de17ee87b3c205
AngelGSoto <gsoto.angel@gmail.com>
2025-06-17 18:10:24 -0300

    Comparing with some lamost spectra

--- Programs/lamost-spectra-plot.py
+++ Programs/lamost-spectra-plot.py
@@ -1,5 +1,6 @@
 """
-Plotting LAMOST spectra for Galactic Sources - Uniform Dashed Lines and Black Labels
+Plotting LAMOST spectra for Galactic Sources 
+Autor: Luis A. Gutiérrez Soto
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,18 +11,18 @@ import argparse
 import os
 
 def plot_spectrum(wavelengths, flux, emission_lines, xlim=None, ylim=None, 
-                  displace_lines=None, displace_lines_x=None, 
+                  displace_lines=None, displace_lines_x=None, inset_positions=None,
                   inset_xlim=None, inset_ylim=None, 
                   inset_xlim2=None, inset_ylim2=None, 
                   savefig=None, title=None, object_name=None):
     """
-    Plots spectrum with uniform dashed emission lines and black labels.
+    Plots spectrum with emission lines labels.
     """
     # Create figure
     fig, ax = plt.subplots(figsize=(14, 6))
-    ax.set_xlabel(r'Observed Wavelength ($\AA$)', fontsize=16)
-    ax.set_ylabel(r'Flux ($10^{-16}$ erg s$^{-1}$ cm$^{-2}$ $\AA^{-1}$)', fontsize=16)
-    ax.tick_params(axis='both', labelsize=14)
+    ax.set_xlabel(r'Wavelength ($\AA$)', fontsize=20)
+    ax.set_ylabel(r'Relative flux', fontsize=20)
+    ax.tick_params(axis='both', labelsize=18)
     
     # Plot the spectrum first
     ax.plot(wavelengths, flux, c="k", lw=1, zorder=3)
@@ -29,14 +30,12 @@ def plot_spectrum(wavelengths, flux, emission_lines, xlim=None, ylim=None,
     # Set axis limits before other elements
     if xlim:
         ax.set_xlim(*xlim)
-    if ylim:
-        ax.set_ylim(*ylim)
     
     # Add title and object name
     if title:
         ax.set_title(title, fontsize=18)
     if object_name:
-        ax.text(0.05, 0.95, object_name, transform=ax.transAxes, 
+        ax.text(0.7, 0.95, object_name, transform=ax.transAxes, 
                 fontsize=16, va='top', ha='left',
                 bbox=dict(facecolor='white', alpha=0.7))
 
@@ -66,9 +65,11 @@ def plot_spectrum(wavelengths, flux, emission_lines, xlim=None, ylim=None,
         y_pos = max_flux[i]
         x_pos = wavelength
         
-        # Apply Y displacement if specified
+        # Modificación clave: Usar porcentaje del rango Y en lugar de valor absoluto
         if displace_lines and label in displace_lines:
-            y_pos += displace_lines[label]
+            # Cambio a desplazamiento relativo
+            y_range = ax.get_ylim()[1] - ax.get_ylim()[0]
+            y_pos += displace_lines[label] * y_range / 100  # displace_lines ahora en % del rango Y
             
         # Apply X displacement if specified
         if displace_lines_x and label in displace_lines_x:
@@ -83,7 +84,7 @@ def plot_spectrum(wavelengths, flux, emission_lines, xlim=None, ylim=None,
             (x_pos, y_pos), 
             alpha=0.9, 
             size=12,
-            xytext=(0, 5),  # Small vertical offset only
+            xytext=(1.5, 5),  # Small vertical offset only
             textcoords='offset points', 
             ha='center', 
             va='bottom',
@@ -92,14 +93,20 @@ def plot_spectrum(wavelengths, flux, emission_lines, xlim=None, ylim=None,
             color="black"  # All labels in black
         )
 
-    # # First inset plot
-    # if inset_xlim and inset_ylim:
-    #     axins = inset_axes(ax, width=2.5, height=2.5, loc='upper right')
-    #     axins.plot(wavelengths, flux, c="k", lw=0.8)
-    #     axins.set_xlim(*inset_xlim)
-    #     axins.set_ylim(*inset_ylim)
-    #     axins.tick_params(labelsize=10)
-    #     mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+    # First inset plot
+    if inset_xlim and inset_ylim:
+        axins = inset_axes(ax, width=2.5, height=2.5, loc='upper right', bbox_to_anchor=inset_positions)
+        axins.plot(wavelengths, flux, c="k", lw=0.8)
+        axins.set_xlim(*inset_xlim)
+        axins.set_ylim(*inset_ylim)
+        axins.tick_params(labelsize=10)
+        mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+        for label_, x in emission_lines.items():
+            axins.annotate(label_, (x, max_flux[list(emission_lines.keys()).index(label_)]), alpha=1, size=14,
+                                xytext=(7.5, 5.6), textcoords='offset points', ha='right', va='bottom',
+                                rotation=90, bbox=bbox_props, zorder=200)
+        axins.tick_params(axis='x', labelsize=14)
+        axins.tick_params(axis='y', labelsize=14)
 
     # # Second inset plot
     # if inset_xlim2 and inset_ylim2:
@@ -110,11 +117,14 @@ def plot_spectrum(wavelengths, flux, emission_lines, xlim=None, ylim=None,
     #     axins2.tick_params(labelsize=10)
     #     mark_inset(ax, axins2, loc1=2, loc2=4, fc="none", ec="0.5")
 
+    if ylim:
+        ax.set_ylim(*ylim)
+
     plt.tight_layout()
     
     if savefig:
         plt.savefig(savefig, dpi=200, bbox_inches='tight')
-        
+    
 
 def main():
     parser = argparse.ArgumentParser(description="Plot LAMOST spectra for Galactic sources")
@@ -142,8 +152,8 @@ def main():
         # Handle different LAMOST data formats
         if len(hdul) > 1 and 'FLUX' in hdul[1].columns.names:  # DR8+ format
             data = hdul[1].data
-            wl = data['WAVELENGTH']
-            flux = data['FLUX']
+            wl = data['WAVELENGTH'].flatten()
+            flux = data['FLUX'].flatten()
         elif len(hdul) > 0 and isinstance(hdul[0].data, np.ndarray):  # Older format
             data = hdul[0].data
             if data.shape[0] >= 3:
@@ -155,7 +165,7 @@ def main():
         else:
             print("Error: Could not read spectrum from FITS file")
             return
-
+    
     # Emission lines (rest frame) - Galactic sources
     emission_lines = {
     "H I 3835": 3835.39,
@@ -169,15 +179,16 @@ def main():
     "Hβ": 4861.33,
     "He I 4922": 4921.93,
     "He I 5016": 5015.68,
-    "[O I] 5577": 5577.34,
     "He I 5876": 5875.624,
     "Hα": 6562.82,
     "He I 6678": 6678.16,
-    "Raman O VI": 6830.0,  # <--- Tu línea en 6831 Å
+    "Raman O VI": 6830.0,  # <--- Pero en el espectro el pica esta en línea en 6831 Å
+        "He I 7065":   7065.25,
+        "O I 7774": 7774.17, # Tengo que verificar esta linea no estoy seguro que sea correcta
         "He I 8445": 8444.69, 
         "H I 8502": 8502.48,  
         "H I 8545": 8545.38,
-        "He I 8608": 8608.31,
+        #"He I 8608": 8608.31,
         "H I 8665": 8665.02,
         "H I 8750": 8750.47,
         "H I 8863": 8862.78,
@@ -187,11 +198,14 @@ def main():
 
     # Custom displacement factors (absolute values in data units)
     displace_lines = {
-        # Example: "Hα": 0.5,  # Move Hα label up by 0.5 flux units
+        "H I 3970": 8.,  # Move Hα label up by 0.5 flux units
+        "Hα": -53,
+        "H I 8502": 9.,
+         #"H I 8665": 5.,
     }
     
     displace_lines_x = {
-        # Example: "[O III] 5007": 5,  # Move label right by 5 Angstroms
+        "Hα": 50,  # Move label right by 5 Angstroms
     }
 
     # Handle ymin/ymax properly
@@ -200,7 +214,7 @@ def main():
     ylim = (ymin, ymax)
 
     # Precompute y-axis limits for insets
-    mask1 = (wl > 4850) & (wl < 5050)
+    mask1 = (wl > 4670) & (wl < 4705)
     mask2 = (wl > 6500) & (wl < 6800)
     
     y_inset1 = 1.2 * np.nanmax(flux[mask1]) if np.any(mask1) else 1.0
@@ -215,8 +229,9 @@ def main():
         ylim=ylim,
         displace_lines=displace_lines,
         displace_lines_x=displace_lines_x,
-        inset_xlim=(4850, 5050),
-        inset_ylim=(0, y_inset1),
+        inset_positions= (-10, 100, 550, 300),
+        inset_xlim=(4670, 4705),
+        inset_ylim=(20, y_inset1),
         inset_xlim2=(6500, 6800),
         inset_ylim2=(0, y_inset2),
         savefig=args.output,
